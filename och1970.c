6e9121cf379cc4f6f97f465240c8c1cf70f9c737
RAW MAIN <rawmain@gmail.com>
2024-06-04 21:44:51 +0200

    Update och1970.c
    och1970.c - edit up to 108

--- och1970.c
+++ och1970.c
@@ -31,3 +31,78 @@
 #include <linux/kthread.h>
 #include <linux/sched/rt.h>
 #include "och1970.h"
+
+static struct och1970_data *t_och1970 = NULL;
+
+static int och1970_i2c_read(uint8_t reg, uint8_t len, uint8_t *data)
+{
+    int err;
+
+    err = i2c_smbus_read_i2c_block_data(t_och1970->client, reg, len, data);
+    if (err < 0) {
+        OCH_ERR("%s: i2c_smbus_read_i2c_block_data failed.", __func__);
+        return err;
+    }
+
+    return 0;
+}
+
+static int och1970_i2c_write(uint8_t reg, uint8_t len, uint8_t *data)
+{
+    int err;
+
+    err = i2c_smbus_write_i2c_block_data(t_och1970->client, reg, len, data);
+    if (err < 0) {
+        OCH_ERR("%s: i2c_smbus_read_i2c_block_data failed.", __func__);
+        return err;
+    }
+
+    return 0;
+}
+
+static void och1970_chip_reset(void)
+{
+    if(gpio_is_valid(t_och1970->rst_gpio)){
+        gpio_set_value(t_och1970->rst_gpio, 1);
+        msleep(20);
+        gpio_set_value(t_och1970->rst_gpio, 0);
+        msleep(20);
+        gpio_set_value(t_och1970->rst_gpio, 1);
+        msleep(200);
+    }
+}
+
+static int check_chip_id(struct och1970_data *och1970)
+{
+    uint8_t chip_id[2];
+
+    och1970_chip_reset();
+
+    och1970_i2c_read(OCH1970_REG_WIA, 2, chip_id);
+
+    OCH_INFO("%s chip_id0: 0x%x chip_id1: 0x%x\n", __func__, chip_id[0], chip_id[1]);
+
+    if ((chip_id[0] == 0x48)&&(chip_id[1] == 0xC0))
+        return 0;
+    else
+        return -EIO;
+}
+
+void och1970_init(void)
+{
+    uint8_t data_temp,now_value;
+
+    och1970_i2c_read(OCH1970_REG_CNTL2, 1, &now_value);
+    data_temp = (now_value & 0xF0) | 0x00;
+    och1970_i2c_write(OCH1970_REG_CNTL2, 1, &data_temp);
+    data_temp |= OCH1970_VALUE_CONTINUOUS_MODE_7_500Hz;
+    och1970_i2c_write(OCH1970_REG_CNTL2, 1, &data_temp);
+
+    och1970_i2c_read(OCH1970_REG_CNTL2, 1, &now_value);
+    data_temp = now_value & ~bit4;
+    och1970_i2c_write(OCH1970_REG_CNTL2, 1, &data_temp);
+
+    och1970_i2c_read(OCH1970_REG_CNTL2, 1, &now_value);
+    data_temp = (now_value & ~bit5) | bit5;
+    och1970_i2c_write(OCH1970_REG_CNTL2, 1, &data_temp);
+}
